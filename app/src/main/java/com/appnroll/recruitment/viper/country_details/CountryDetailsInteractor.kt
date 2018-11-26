08aab2f92eb0c0731db9e8d33499d887193296e5
Aleksandra Mikusek <xamikusek@gmail.com>
2018-11-27 00:42:53 +0100

    [country-details] remove: redundant layers

--- app/src/main/java/com/appnroll/recruitment/viper/country_details/CountryDetailsInteractor.kt
+++
@@ -1,5 +0,0 @@
-package com.appnroll.recruitment.viper.country_details
-
-import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor
-
-class CountryDetailsInteractor : BaseRxInteractor(), CountryDetailsContract.Interactor
--- app/src/main/java/com/appnroll/recruitment/viper/country_details/CountryDetailsRouting.kt
+++
@@ -1,7 +0,0 @@
-package com.appnroll.recruitment.viper.country_details
-
-import android.app.Activity
-
-import com.mateuszkoslacz.moviper.base.routing.BaseRxRouting
-
-class CountryDetailsRouting : BaseRxRouting<Activity>(), CountryDetailsContract.Routing
