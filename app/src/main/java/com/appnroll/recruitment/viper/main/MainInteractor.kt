3320fc5de1c5f106b1417c5ea33ba26c6b5d4c1d
Aleksandra Mikusek <xamikusek@gmail.com>
2018-11-26 20:42:31 +0100

    [main] add: empty viper implementation

---
+++ app/src/main/java/com/appnroll/recruitment/viper/main/MainInteractor.kt
@@ -0,0 +1,5 @@
+package com.appnroll.recruitment.viper.main
+
+import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor
+
+class MainInteractor : BaseRxInteractor(), MainContract.Interactor
---
+++ app/src/main/java/com/appnroll/recruitment/viper/main/MainRouting.kt
@@ -0,0 +1,7 @@
+package com.appnroll.recruitment.viper.main
+
+import android.app.Activity
+
+import com.mateuszkoslacz.moviper.base.routing.BaseRxRouting
+
+class MainRouting : BaseRxRouting<Activity>(), MainContract.Routing
